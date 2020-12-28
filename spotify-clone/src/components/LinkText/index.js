e6b0a5049cf5596359e68629b051dafb117abadf
KantKontrol <nick.derissio.tech@gmail.com>
2020-12-27 21:04:46 -0500

    removed url from link text comp. created api util

--- spotify-clone/src/components/LinkText/index.js
+++ spotify-clone/src/components/LinkText/index.js
@@ -3,7 +3,7 @@ import "./index.css";
 
 export default function LinkText(props){
 
-    const {text, url} = props;
+    const {text} = props;
 
     return(
         <button className="link-text-style" >
